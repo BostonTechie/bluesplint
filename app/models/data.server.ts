85a41371f6f52dd8e494df69b2992c9d92fead3c
andrew urquhart <andrewurq@gmail.com>
2022-08-09 15:54:57 -0400

    making accounting route

--- app/models/data.server.ts
+++ app/models/data.server.ts
@@ -1,3 +1,11 @@
-import type { Hive } from "~db/server" 
+import type { hive } from "~db/server" 
+            
+import { prisma } from "~/db.server";
 
-export type {Hive} from "@prisma/client"
+export type {hive} from "@prisma/client"
+
+export function getDataItems() {
+    return prisma.hive.findMany({
+      take: 10,
+    });
+  }
